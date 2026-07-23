7ea564a5cf1c3bc306efe5b361304f6ae0ef8741
Bhasker Kamshetty <30320555+bhaskerkamshetty@users.noreply.github.com>
2026-07-23 21:01:15 +0530

    Enhance package installation script for Ubuntu
    Updated the script to install additional packages including management, monitoring, and security tools.

--- linux/ubuntu/basic-packages-server.sh
+++ linux/ubuntu/basic-packages-server.sh
@@ -5,29 +5,21 @@
 #Date: 9th March 2024
 ############################################################
 
-#Installs Nano Text Editor
-echo "Installing Nano & Vim Text Editor"
-apt-get install nano vim -y
-
-#Installs Wget Tool
-echo "Installing Wget & Curl Tool"
-apt-get install wget curl -y
-
-#Installs Zip/Unzip Tool
-echo "Installing Zip/Unzip Tool"
-apt-get install zip unzip -y
-
 #Installs Network Tools
 echo "Installing Network Tools"
-apt-get install net-tools iputils-ping telnet -y
+apt-get install wget curl net-tools telnet iputils-ping mtr-tiny iproute2 iperf3 cockpit -y
 
-#Installs Git
-echo "Installing Git"
-apt-get install git -y
+#Installs Management & Session Persistence
+echo "Installing Management & Session Persistence Tools"
+apt-get install rsync nano vim zip unzip git micro tmux -y
 
-#Installs Cockpit
-echo "Installing Cockpit"
-apt-get install cockpit -y
+#Installs System Monitoring & Diagnostics
+echo "Installing System Monitoring & Diagnostics Tools"
+apt-get install htop ncdu iotop -y
+
+#Installs Security & System Hardening
+echo "Installing Security & System Hardening Tools"
+apt-get install fail2ban unattended-upgrades ufw iotop -y
 
 #Conclusion
 echo "Done. Installing"
